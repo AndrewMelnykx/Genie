b6b4bc4a847270ea88a18036a79fc806d66ac492
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-12-28 21:13:41 +0100

    Changed Prisma logic

--- client/lib/db.ts
+++ client/lib/db.ts
@@ -1,17 +1,25 @@
+// import { PrismaClient } from "@/lib/generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+// });
+// const prismaClientSingleton = () => {
+//   return new PrismaClient({ adapter });
+// };
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+// const prismadb = globalThis.prismaGlobal ?? prismaClientSingleton();
+// if (process.env.NODE_ENV !== "production") {
+//   globalThis.prismaGlobal = prismadb;
+// }
+// export default prismadb;
+
 import { PrismaClient } from "@/lib/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter });
-};
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-const prismadb = globalThis.prismaGlobal ?? prismaClientSingleton();
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prismaGlobal = prismadb;
-}
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prismadb = new PrismaClient({ adapter });
+
 export default prismadb;
--- client/prisma.config.ts
+++ client/prisma.config.ts
@@ -1,13 +1,22 @@
+// import "dotenv/config";
+// import { defineConfig, env } from "prisma/config";
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: "prisma/migrations",
+//   },
+//   datasource: {
+//     url: env("DATABASE_URL"),
+//     shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+//   },
+// });
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
-    url: env("DATABASE_URL"),
-    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+    url: env("DIRECT_URL"),
   },
 });
