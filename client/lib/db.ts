ce12990e253f5b9938c5e61a4cec5410c7c1d54b
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-12-28 21:51:48 +0100

    Fixed logic for prisma

--- client/lib/db.ts
+++ client/lib/db.ts
@@ -1,25 +1,13 @@
-// import { PrismaClient } from "@/lib/generated/prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL,
-// });
-// const prismaClientSingleton = () => {
-//   return new PrismaClient({ adapter });
-// };
-// declare const globalThis: {
-//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-// } & typeof global;
-// const prismadb = globalThis.prismaGlobal ?? prismaClientSingleton();
-// if (process.env.NODE_ENV !== "production") {
-//   globalThis.prismaGlobal = prismadb;
-// }
-// export default prismadb;
-
 import { PrismaClient } from "@/lib/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prismadb = new PrismaClient({ adapter });
+
+declare const globalThis: {
+  prismaGlobal?: PrismaClient;
+};
+
+const prismadb = globalThis.prismaGlobal ?? new PrismaClient({ adapter });
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prismadb;
 
 export default prismadb;
