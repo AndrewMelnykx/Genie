00375a0c02b54faf6c215246994ba3cd05d7d0a6
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-18 22:29:22 +0100

    Changed prisma directory

--- client/prisma.config.ts
+++ client/prisma.config.ts
@@ -1,17 +1,7 @@
-// import { defineConfig } from "prisma/config";
-
-// export default defineConfig({
-//   schema: "./schema.prisma",
-//   datasource: {
-//     url: process.env.DATABASE_URL,
-//     shadowDatabaseUrl: process.env.DIRECT_URL,
-//   },
-// });
-
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "./schema.prisma",
+  schema: "./prisma/schema.prisma",
   datasource: {
     url: process.env.DATABASE_URL,
     shadowDatabaseUrl: process.env.DIRECT_URL,
