7b6c20c24c4cb202bc1f8d88c7fbb3d601427ac8
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-18 22:26:20 +0100

    CHnaged prisma config

--- client/prisma.config.ts
+++ client/prisma.config.ts
@@ -1,3 +1,13 @@
+// import { defineConfig } from "prisma/config";
+
+// export default defineConfig({
+//   schema: "./schema.prisma",
+//   datasource: {
+//     url: process.env.DATABASE_URL,
+//     shadowDatabaseUrl: process.env.DIRECT_URL,
+//   },
+// });
+
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
